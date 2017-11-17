32b05ac20f25b3cfce82e227140eb37e0bdb7189
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-11-18 01:32:09 +0200

    Dare

--- Sawa/app/src/main/java/com/example/zodiac/sawa/RecyclerViewAdapters/HomePostAdapter.java
+++ Sawa/app/src/main/java/com/example/zodiac/sawa/RecyclerViewAdapters/HomePostAdapter.java
@@ -1,8 +1,32 @@
 package com.example.zodiac.sawa.RecyclerViewAdapters;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by Rabee on 11/17/2017.
  */
 
-public class HomePostAdapter {
+public class HomePostAdapter extends RecyclerView.Adapter<HomePostAdapter.RecyclerViewHolder> {
+    @Override
+    public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class RecyclerViewHolder extends RecyclerView.ViewHolder  {
+        public RecyclerViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
